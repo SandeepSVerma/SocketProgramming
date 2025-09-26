26900841f3d4b497ebd0f5608d20f14bcd1b07c0
SandeepSVerma <54927594+SandeepSVerma@users.noreply.github.com>
2025-09-26 11:53:20 +0530

    Add files via upload

---
+++ CryptoHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSocketProgramming
+{
+    public static class CryptoHelper
+    {
+        public static readonly byte[] key = Encoding.UTF8.GetBytes("12345678123456781234567812345678"); //32 bytes AES-25
+        public static readonly byte[] iv = Encoding.UTF8.GetBytes("1234567812345678"); //16 bytes AES
+
+        public static byte[] Encrypt(string text)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                using (var ms = new MemoryStream())
+                using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                using (var sw = new StreamWriter(cs))
+                {
+                    sw.Write(text);
+                    sw.Flush();
+                    cs.FlushFinalBlock();
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        public static string Decrypt(byte[] cipher)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                using (var ms = new MemoryStream(cipher))
+                using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                using (var sr = new StreamReader(cs))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
+    }
+}
